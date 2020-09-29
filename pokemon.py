6f237b82e7cf74479835582f152671a8259edd2e
Antoine Washington <antoine.washington98@gmail.com>
2020-09-29 12:03:33 -0400

    started a to-d list

--- pokemon.py
+++ pokemon.py
@@ -4,6 +4,9 @@
 #normal 0,fire 1,water 2,grass 3,electric 4,ice 5,fighting 6,poison 7,
 #ground 8,flying 9,psychic 10,bug 11, #rock 12,ghost 13,dragon 14,
 #dark 15,steel 16,fairy 17
+#*********to do list: natures, terrain, 
+#
+#
 
 import numpy as np
 import astropy.table as tbl
