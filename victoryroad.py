74d0cff0685584746d7f09985a3317c624511e24
Antoine <antoine.washington98@gmail.com>
2022-11-10 21:07:12 -0500

    spoilers

---
+++ victoryroad.py
@@ -0,0 +1,37 @@
+#Antoine
+#this script will use base_pokemon, moves, and dexpoke, to create 5 specific pokemon parties for pokemon.py
+#let's go!
+import copy
+import numpy as np
+from base_pokemon import mon, makeMon, makeRandom
+from moves import mov,struggle
+from dexpoke import dex
+
+#silver
+c1_name = "Silver of Johto"
+
+
+
+#zinnia
+c2_name = "Zinnia of the Draconids"
+
+
+
+#cynthia
+c3_name = "Cynthia the Sinnoh Champion"
+
+
+
+#N
+c4_name = "Former King N of Unova"
+
+
+
+#Champ
+c5_name = "Pokemon Trainer Red"
+
+
+if __name__ == "__main__":
+    pass
+else:
+    pass
