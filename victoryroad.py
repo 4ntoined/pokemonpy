e1eb0e27f449385c3d566d9e9ffa486ba48050b3
Antoine <antoine.washington98@gmail.com>
2022-11-10 22:44:01 -0500

    e4 #3

--- victoryroad.py
+++ victoryroad.py
@@ -60,18 +60,48 @@ zin1 = makeMon(372,level=levil+3,nacher=(4, 1)) #salamence
 zin2 = makeMon(696,level=levil+3,nacher=(1, 3)) #tyran
 zin3 = makeMon(705,level=levil+0,nacher=(3, 1)) #goo
 zin4 = makeMon(910,level=levil+5,nacher=(1, 1)) #zy
+#evs ivs
 
+#moves
 
-#cynthia, spiritomb, lucario, milotic, giratina-origin(register)
+#partyfill
+c2_party = [zin1,zin2,zin3,zin4]
+#cynthia, spiritomb 441, 447 lucario, milotic 349, giratina-origin(register) 911
+cyn1 = makeMon(441,level=levil+0,nacher=(2, 0)) #spirit
+cyn2 = makeMon(447,level=levil+3,nacher=(4, 3)) #luke
+cyn3 = makeMon(349,level=levil+3,nacher=(3, 0)) #nilo
+cyn4 = makeMon(911,level=levil+5,nacher=(4, 2)) #gira
+#evs ivs
 
+#moves
 
+#partyfill
+c3_party = [cyn1,cyn2,cyn3,cyn4]
+#N, 643 zekrom, 583 vanilluxe,566  archeops, zoroark 570 (no illusion :() 
+nnn1 = makeMon(643,level=levil+8,nacher=(2, 0)) #spirit
+nnn2 = makeMon(583,level=levil+3,nacher=(4, 3)) #vanil
+nnn3 = makeMon(566,level=levil+5,nacher=(3, 0)) #arch
+nnn4 = makeMon(570,level=levil+5,nacher=(4, 2)) #zoro
 
-#N, zekrom, vanilluxe, archeops, zoroark (no illusion :() 
+#evs ivs
 
+#moves
 
+#partyfill
+c4_party = [nnn1,nnn2,nnn3,nnn4]
+#Champ,2 venusaur, 24 pikachu, 5 charizard, 8 blastoise, 913 mega-mewtwo Y(register), mew 150
+grn1 = makeMon(2,level=levil+4,nacher=(4, 0)) #venu
+grn2 = makeMon(24,level=levil+8,nacher=(4, 3)) #pika
+grn3 = makeMon(5,level=levil+4,nacher=(3, 3)) #char
+grn4 = makeMon(8,level=levil+4,nacher=(1, 2)) #blas
+grn5 = makeMon(913,level=levil+9,nacher=(3, 0)) #mew2
+grn6 = makeMon(150,level=levil+10,nacher=(0, 0)) #mew
+#evs ivs
 
-#Champ,venusaur, pikachu, charizard, blastoise, mega-mewtwo Y(register), mew
+#movs
 
+#partyfilling
+c5_party=[grn1,grn2,grn3,grn4,grn5,grn6]
 
 if __name__ == "__main__":
     pass
